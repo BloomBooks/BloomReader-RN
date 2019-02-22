cb892d638bf2f084ddacdc009edf3e690fff1dd6
Rick Conrad <richardjconrad@gmail.com>
2019-02-22 14:25:23 +0100

    Use version from package.json as the master version number for the app. Bundle Moon & Cap with the app.

--- app/util/startupTasks.ts
+++ app/util/startupTasks.ts
@@ -1,5 +1,25 @@
 import * as BookStorage from "./BookStorage";
+import { AsyncStorage } from "react-native";
+import importSampleBooks from "./importSampleBooks";
+
+const appVersion = require("../../package.json").version;
+const lastRunVersionKey = "bloomreader.lastRunVersion";
 
 export default async function startupTasks(): Promise<void> {
-  await BookStorage.createDirectories(); // Make this run only on new-install?
+  await BookStorage.createDirectories();
+
+  const lastRunVersion = await getLastRunVersion();
+  switch (lastRunVersion) {
+    case null: // First run
+      await importSampleBooks();
+  }
+  setLastRunVersion();
+}
+
+async function getLastRunVersion() {
+  return AsyncStorage.getItem(lastRunVersionKey);
+}
+
+async function setLastRunVersion() {
+  AsyncStorage.setItem(lastRunVersionKey, appVersion);
 }
