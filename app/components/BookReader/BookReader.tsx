87dff03408f42024566dd8ebe3f23c0abdf5c318
John Thomson <john_thomson@sil.org>
2019-03-27 14:50:18 -0500

    Use real BloomPlayer code

--- app/components/BookReader/BookReader.tsx
+++ app/components/BookReader/BookReader.tsx
@@ -14,7 +14,7 @@ export interface IProps {
 
 export interface IState {
   bookPath?: string;
-  html?: string;
+  bookReady?: boolean;
 }
 
 export default class BookReader extends React.PureComponent<IProps, IState> {
@@ -25,21 +25,37 @@ export default class BookReader extends React.PureComponent<IProps, IState> {
   async componentDidMount() {
     const bookPath = await BookStorage.openBookForReading(this.book());
     this.setState({ bookPath: bookPath });
-    this.loadHtml();
+    this.loadBook();
   }
 
-  async loadHtml() {
-    const html = await BookStorage.fetchHtml();
-    this.setState({ html: html });
+  async loadBook() {
+    // book is now called openBook.htm in temp folder.
+    // (We are taking advantage of bloom-player's assumption that the htm file has the
+    // same name as the folder. Although it's a slight nuisance to have to rename it,
+    // it means that the book we want to play is always at the same exact location,
+    // so we don't have to come up with a mechanism to pass the html file path to
+    // the player.)
+    await BookStorage.moveBook();
+    this.setState({ bookReady: true });
   }
 
+  // The player-webview.htm file loads in the bloom-player-react code to display the book
+  // in the indicated folder. Since the player loads the book's HTML from a file:/// url
+  // not in the assets folder where player-webview.htm lives, and then proceeds to
+  // similarly load images and other resources from that folder, we are violating
+  // cross-domain rules. However, WebView allows us to turn that off. I'm not sure
+  // all the properties used here are necessary to prevent problems, but probably
+  // at least some of them are.
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <Text>Reading {this.book().name} Put a Bloom Player here vvv</Text>
-        {!!this.state.html && (
-          <WebView source={{ html: this.state.html }} />
-          // <Text>{this.state.html}</Text>
+        {this.state.bookReady && (
+          <WebView source={{ uri: "file:///android_asset/bloom-player/player-webview.htm" }}
+            mixedContentMode="always"
+            allowUniversalAccessFromFileURLs={true}
+            allowFileAccess={true}
+            javaScriptEnabled={true}
+            />
         )}
         <DrawerLocker
           setDrawerLockMode={this.props.screenProps.setDrawerLockMode}
--- app/util/BookStorage.ts
+++ app/util/BookStorage.ts
@@ -107,6 +107,16 @@ export async function getThumbnail(
   };
 }
 
+export async function moveBook(): Promise<string> {
+  const fileList = await RNFS.readDir(openBookDir);
+  const htmlFile = fileList.find(entry => /\.html?$/.test(entry.name));
+  if (!htmlFile || !htmlFile.path) {
+    return "";
+  }
+  await RNFS.moveFile(htmlFile.path, openBookDir + "/openBook.htm");
+  return htmlFile.path;
+}
+
 export async function fetchHtml(): Promise<string> {
   const fileList = await RNFS.readDir(openBookDir);
   const htmlFile = fileList.find(entry => /\.html?$/.test(entry.name));
